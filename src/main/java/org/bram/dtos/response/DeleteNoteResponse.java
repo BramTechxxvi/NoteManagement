2e4b95c83d1c6a38a68e032de9b9065c58bab958
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-19 13:20:05 +0100

    new

--- src/main/java/org/bram/dtos/response/DeleteNoteResponse.java
+++ src/main/java/org/bram/dtos/response/DeleteNoteResponse.java
@@ -1,12 +1,11 @@
 package org.bram.dtos.response;
 
 import lombok.Data;
-import org.springframework.data.annotation.Id;
 
 @Data
 public class DeleteNoteResponse {
 
     private String message;
-    
+    private boolean success;
 
 }
