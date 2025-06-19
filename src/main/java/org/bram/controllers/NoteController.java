3791973c158e6f07dadafe7d9f04354522f30480
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-19 13:57:16 +0100

    new

--- src/main/java/org/bram/controllers/NoteController.java
+++ src/main/java/org/bram/controllers/NoteController.java
@@ -6,10 +6,7 @@ import org.bram.exceptions.NoteNotFoundException;
 import org.bram.services.NoteServices;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/notes")
@@ -35,5 +32,5 @@ public class NoteController {
         }
     }
 
-    @
+    @GetMapping("/getANote")
 }
