73d7824a36b8e1cff7690a44eae13fb9ca0db067
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-19 13:21:33 +0100

    new

--- src/main/java/org/bram/dtos/request/DeleteNoteRequest.java
+++ src/main/java/org/bram/dtos/request/DeleteNoteRequest.java
@@ -1,8 +1,11 @@
 package org.bram.dtos.request;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 @Data
 public class DeleteNoteRequest {
-    @
+    @Id
+    private String id;
+    private
 }
