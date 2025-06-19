1d5c0ffdb157702ae7b604e47cdc22a88d77af82
BramTechxxvi <bramtechxxvi@gmail.com>
2025-06-19 14:27:58 +0100

    new

--- src/main/java/org/bram/controllers/NoteController.java
+++ src/main/java/org/bram/controllers/NoteController.java
@@ -57,7 +57,7 @@ public class NoteController {
 
     @PutMapping("/update/{id}")
     public ResponseEntity<UpdateNoteResponse> updateNote(@PathVariable("id") String id, @RequestBody UpdateNoteRequest request) {
-        
+        request.setNoteId();
         UpdateNoteResponse response = noteServices.updateNote(request);
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
