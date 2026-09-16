222594ffee859eabf86f71d53930a1859b4c488f
gamp <bramtechxxvi@gmail.com>
2026-09-16 15:03:37 +0100

    changes

--- src/main/java/org/bram/data/models/Note.java
+++ src/main/java/org/bram/data/models/Note.java
@@ -1,12 +1,15 @@
 package org.bram.data.models;
 
-import lombok.Data;
+
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
-@Data
+@Setter
+@Getter
 @Document(collection="Notes")
 public class Note {
 
